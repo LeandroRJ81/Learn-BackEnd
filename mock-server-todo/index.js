bdb2c57a2656c8d565b253418f15fe1ba608d07c
LeandroRJ81 <leandrogt22@hotmail.com>
2025-03-19 15:37:06 -0300

    feat-integration

--- mock-server-todo/index.js
+++ mock-server-todo/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const { v4: uuidv4 } = require('uuid')
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 
 const tasks = []
@@ -10,12 +13,28 @@ app.get('/tasks', (req, res) => {
 })
 
 app.post('/tasks', (req, res) => {
-    const { task } = req.body
+    const { text } = req.body
 
-    const newTask = { id: uuidv4(), task }
+    const newTask = { id: uuidv4(), text }
     tasks.push(newTask)
 
     return res.json(newTask)
+})
+
+app.put('/tasks/:id', (req,res) => {
+    const { id } = req.params
+    const { completed } = req.body
+
+    const taskIndex = tasks.findIndex(task => task.id === id)
+
+    if(taskIndex < 0) {
+        return res.status(400).json({ error: "Task not found."  })
+    }
+
+    tasks[taskIndex].completed = completed
+
+    return res.json(tasks[taskIndex])
+    
 })
 
 app.delete('/tasks/:id', (req, res) => {
@@ -29,7 +48,7 @@ app.delete('/tasks/:id', (req, res) => {
 
     tasks.splice(taskIndex, 1)
 
-    return res.json({ message: "Task removed successfully" })
+    return res.status(200).json({ message: "Task removed successfully" })
 
 })
 
